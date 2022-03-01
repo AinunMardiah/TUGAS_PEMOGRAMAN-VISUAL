602aa5206021d2094dac95883cdec7e72a582259
AinunMardiah <85837195+AinunMardiah@users.noreply.github.com>
2022-03-01 21:45:24 +0700

    Add files via upload

---
+++ Tugas1.2.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: AINUN HUSIN
+ * Date: 27/02/2022
+ * Time: 10:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tugas1.pemograman_visual
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Nama Kamu Siapa ? ");
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama Kamu "+nama);
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
